08787fcae76398e14f9fb85910d21ec3a90962f5
Flyingsparkler <yiyi83@hotmail.com>
2023-11-29 17:21:11 +0800

    commit

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -1,4 +1,41 @@
-import OpenAI from "openai";
+import {NextApiRequest, NextApiResponse} from 'next';
+import OpenAI from 'openai';
+
+/*const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+*/
+
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"],
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.body.prompt !== undefined) {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: `${req.body.prompt}` }],
+      //instruction: prompt,
+      //max_tokens: 1000,   
+
+      //model: "text-davinci-003",
+      //prompt: `${req.body.prompt}`,
+    });
+    console.log(completion.choices[0].message.content);
+    res.status(200).json({ text: `${completion.choices[0].message.content}` });
+  } else {
+    //console.log(nextapo)
+    res.status(400).json({ text: "No prompt provided." });
+  }
+};
+
+
+/*import OpenAI from "openai";
 //import type { NextApiRequest, NextApiResponse } from 'next';
 
 //export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -23,3 +60,4 @@ export default async (req: any, res: any) => {
     res.status(400).json({ text: "No prompt provided." });
   }
 };
+*/
