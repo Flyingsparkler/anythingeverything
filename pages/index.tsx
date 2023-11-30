daaa1a1c17f1486db45c5af79246358ae9ffb8a5
Flyingsparkler <yiyi83@hotmail.com>
2023-11-30 09:01:39 +0800

    commit message

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,30 +26,39 @@ export default function Page() {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    const prompt = "Hello ChatGPT, please suggest a " + formData.noOfDays + " days meals plan with recipes for a " + formData.diet + " with cuisines, " + formData.cuisine + " in markdown format.";
-    setResponse("");
-    console.log("Getting response from OpenAI"); 
-    console.log("prompt" + prompt)
-    setIsLoading(true)
+    if (formData.noOfDays != "" && formData.cuisine != "" && formData.diet != "") {
+        const prompt = "Hello ChatGPT, please suggest a " + formData.noOfDays + " days meals plan with recipes for a " + formData.diet + " with cuisines, " + formData.cuisine + " in markdown format.";
+        setResponse("");
+        console.log("Getting response from OpenAI"); 
+        console.log("prompt" + prompt)
+        setIsLoading(true)
+        setErr("");
 
-    fetch('/api/submit', {
-      method: 'POST',
-      /*headers: {
-        'Content-Type': 'application/json',
-      },*/
-      body: JSON.stringify({prompt: prompt}),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setIsLoading(false)
-        console.log(data.text);
-        setResponse(data.text);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+        fetch('/api/submit', {
+          method: 'POST',
+          /*headers: {
+            'Content-Type': 'application/json',
+          },*/
+          body: JSON.stringify({prompt: prompt}),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setIsLoading(false)
+            console.log(data.text);
+            const messagecontent = data.text; 
+            setResponse(messagecontent.replace(" in markdown format", ""));
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+      else 
+      {
+        setErr("Please provide input for all fields.")
+      }
   };
   return (
+    
     <div className="container">
       <div className="row">
         <div className="col">
@@ -59,22 +68,22 @@ export default function Page() {
           {err && <div style={{ color: 'red' }}>{err}</div>}
           </div>
           <form onSubmit={handleSubmit} className="form-horizontal">
+          <div className="form-group">
+              <label htmlFor="Diet" className="col-sm-2 control-label">Diet</label>
+              <div className="col-sm-10">
+                <input type="text" className="form-control" id="diet" name="diet" placeholder="Anything or Vegan or Vegetarian or Pescatarian or Paleo or Ketogenic" value={formData.diet} onChange={handleInputChange} />
+              </div>
+            </div>
             <div className="form-group">
               <label htmlFor="Cuisine" className="col-sm-2 control-label">Cuisine(s)</label>
               <div className="col-sm-10">
                 <input type="text" className="form-control" id="cuisine" name="cuisine" placeholder="Japanese, Teochew, Chinese, Korean etc" value={formData.cuisine} onChange={handleInputChange}/>
               </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="Diet" className="col-sm-2 control-label">Diet</label>
-              <div className="col-sm-10">
-                <input type="text" className="form-control" id="diet" name="diet" placeholder="" value={formData.diet} onChange={handleInputChange} />
-              </div>
-            </div>
             <div className="form-group"> 
               <label htmlFor="NoOfDays" className="col-sm-2 control-label">No. of Day(s)</label>
               <div className="col-sm-10">
-                <input type="number" className="form-control" id="noOfDays" name="noOfDays" placeholder="" pattern="[0-9]{1}" value={formData.noOfDays} onChange={handleInputChange} />
+                <input type="number" className="form-control" id="noOfDays" name="noOfDays" placeholder="" min="0" max="7" value={formData.noOfDays} onChange={handleInputChange} />
               </div>
             </div>
             <br/>
@@ -90,7 +99,7 @@ export default function Page() {
       </div>
       <div className="row">
         <div className="col">
-        <br/><br/><ReactMarkdown>{response}</ReactMarkdown>
+        <br/><br/><ReactMarkdown className="fs-6">{response}</ReactMarkdown>
         </div> 
       </div>
     </div>
